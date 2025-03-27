10f4c5781c289a61e56af60b25c11dd4e7e9f67a
snhasler <samantha.n.hasler@gmail.com>
2025-03-27 12:23:04 -0400

    add astrometric error due to photometry calculation

--- test_deconfuser_wPhotometry.py
+++ test_deconfuser_wPhotometry.py
@@ -39,11 +39,11 @@ parser.add_argument("--max_i", type=float, default=np.pi/2, help="maximum inclin
 parser.add_argument("--max_e", type=float, default=0.3, help="maximum eccentricity (default: 0.3)")
 parser.add_argument("--spread_i_O", type=float, default=0.0, help="spread of inclination and LAN in radians (default: 0.0 - coplanar)")
 parser.add_argument("--n_systems", type=int, default=10, help="number of systems (default: 10)")
-parser.add_argument("--sigma_photo", action="store_true", help="whether to add astrometric uncertainty due to photometry")
-parser.add_argument("-v", "--verbose", action="store_true", help="print planet data")
-parser.add_argument("tolerances", type=float, nargs="+", help="orbit fit tollerances")
 parser.add_argument("--output_file", type=str, default=f"test_deconfuser_output_{now}.txt", help="deconfuser output results file name")
 parser.add_argument("--ranking_path", type=str, default="output_files/ranking_files/", help="path for output ranking files")
+parser.add_argument("-sigma_photo", action="store_true", help="whether to add astrometric uncertainty due to photometry")
+parser.add_argument("-v", "--verbose", action="store_true", help="print planet data")
+parser.add_argument("tolerances", type=float, nargs="+", help="orbit fit tollerances")
 args = parser.parse_args()
 
 # Create text file and log file for output
@@ -80,7 +80,6 @@ planet = phot.Planet(R_p=6.371e6, Ag=0.3)                         # values for E
 detector = phot.Detector(qe=0.837, cic=0.016, dark_current=1.3e-4, read_noise=120, gain=1000, 
                     fwc=80000, conversion_gain=1.0, t=108e3, D=2.36, throughput=0.38, f_pa=0.039,
                     wavelength=573.8e-9, bandwidth=56.5e-9, stability_constant=1.0) # Roman instrument parameters -- TODO: change t back to 3600
-print('estimated_FWHM: ', detector.FWHM, ' mas')
 
 # Observation epochs (years)
 ts = args.cadence*np.arange(args.n_epochs)
@@ -113,7 +112,6 @@ for _ in range(args.n_systems):
     observations = np.stack([xs,ys,zs], axis=2).reshape((-1,3))
 
     # Add radially bounded astrometry error
-    # TODO: ADD CHANGES FOR NOISE BASED ON PHOTOMETRY HERE
     noise_r = tolerances[-1]*np.random.random(len(observations)) 
     noise_a = 2*np.pi*np.random.random(len(observations))
     observations[:,0] += noise_r*np.cos(noise_a) # x-direction error 
@@ -127,12 +125,15 @@ for _ in range(args.n_systems):
     # get noisy and not noisy photometric detections for simulated system
     noisy_detections, detections_photon_rates, SNRs = phot.get_detections_counts(args.n_planets, args.n_epochs, xyzs=all_coords, 
                                                                                Planet=planet, Star=star, Detector=detector)
-    print('SNRs: ', SNRs)
     
     if args.sigma_photo: # if true, add astrometric uncertainty to observations due to simulated photometry
-        sigma_mas = phot.sigma_photo()
-
-
+        # Calculate error in x,y direction due to planet signal
+        x_err, y_err = phot.astro_photo_uncertainty(SNRs, detector, star)
+        z_err = np.zeros_like(x_err)
+        photo_astro_errs = np.stack([x_err,y_err,z_err], axis=2).reshape((-1,3))
+        # Add error to simulated observations' coordinates
+        observations[:,0] += photo_astro_errs[:,0]
+        observations[:,1] += photo_astro_errs[:,1]
 
     if args.verbose:
         print("\nts =", list(ts)) 
@@ -266,3 +267,4 @@ f.close()
 logfile.close() 
 sys.stdout = sys.__stdout__ # reset standard output to terminal
 sys.stderr = sys.__stderr__ # reset error output to terminal
+print('Output written to: output_files/' + args.output_file + " & " + args.ranking_path)
